53603d5608b08d997db91b42acc5c3885198c0fc
Santosh Krishnamurthy <san.b350@gmail.com>
2023-03-27 06:19:20 -0500

    Initial Commit

---
+++ proto-autolayout-swift-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proto-autolayout-swift-ios
+//
+//  Created by Santosh Krishnamurthy on 3/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
